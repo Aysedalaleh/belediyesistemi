0b7d9662d8248a5257c6d765cf4ac5642c9a5d12
ayse <aishadalala99@gmail.com>
2022-06-15 21:43:25 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login-register/login-register.module').then(m => m.LoginRegisterPageModule)
+  },
+  {
+    path: 'about-us',
+    loadChildren: () => import('./pages/about-us/about-us.module').then(m => m.AboutUsPageModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./pages/news/news.module').then(m => m.NewsPageModule)
+  },
+  {
+    path: 'news-details',
+    loadChildren: () => import('./pages/news-details/news-details.module').then(m => m.NewsDetailsPageModule)
+  },
+  {
+    path: 'contact-us',
+    loadChildren: () => import('./pages/contact-us/contact-us.module').then(m => m.ContactUsPageModule)
+  },
+  {
+    path: 'send-complaint',
+    loadChildren: () => import('./pages/send-complaint/send-complaint.module').then(m => m.SendComplaintPageModule)
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,45 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+//Translation
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { MainNavComponentModule } from './components/main-nav/main-nav.module';
+import { NavbarComponentModule } from './components/navbar/navbar.module';
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot({
+      backButtonText: '',
+      mode: 'ios',
+      animated: false
+    }),
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'tr',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+    MainNavComponentModule,
+    NavbarComponentModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
